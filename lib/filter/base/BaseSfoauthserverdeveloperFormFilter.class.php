2b9fe38463451aa9a31046475820a731c2206da6
lmaxim <lmaxim@lmaxim-work.(none)>
2012-05-05 15:43:30 +0400

    renamed to migration

--- lib/filter/base/BaseSfoauthserverdeveloperFormFilter.class.php
+++ lib/filter/base/BaseSfoauthserverdeveloperFormFilter.class.php
@@ -3,45 +3,45 @@
 /**
  * Sfoauthserverdeveloper filter form base class.
  *
- * @package	##PROJECT_NAME##
+ * @package    ##PROJECT_NAME##
  * @subpackage filter
- * @author	 ##AUTHOR_NAME##
+ * @author     ##AUTHOR_NAME##
  */
 abstract class BaseSfoauthserverdeveloperFormFilter extends BaseFormFilterPropel
 {
-	public function setup()
-	{
-		$this->setWidgets(array(
-							   'consumer_id' => new sfWidgetFormPropelChoice(array('model' => 'Sfoauthserverconsumer', 'add_empty' => true)),
-							   'user_id' => new sfWidgetFormPropelChoice(array('model' => 'sfGuardUser', 'add_empty' => true)),
-							   'admin' => new sfWidgetFormChoice(array('choices' => array('' => 'yes or no', 1 => 'yes', 0 => 'no'))),
-						  ));
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'consumer_id' => new sfWidgetFormPropelChoice(array('model' => 'Sfoauthserverconsumer', 'add_empty' => true)),
+      'user_id'     => new sfWidgetFormPropelChoice(array('model' => 'sfGuardUser', 'add_empty' => true)),
+      'admin'       => new sfWidgetFormChoice(array('choices' => array('' => 'yes or no', 1 => 'yes', 0 => 'no'))),
+    ));
 
-		$this->setValidators(array(
-								  'consumer_id' => new sfValidatorPropelChoice(array('required' => false, 'model' => 'Sfoauthserverconsumer', 'column' => 'id')),
-								  'user_id' => new sfValidatorPropelChoice(array('required' => false, 'model' => 'sfGuardUser', 'column' => 'id')),
-								  'admin' => new sfValidatorChoice(array('required' => false, 'choices' => array('', 1, 0))),
-							 ));
+    $this->setValidators(array(
+      'consumer_id' => new sfValidatorPropelChoice(array('required' => false, 'model' => 'Sfoauthserverconsumer', 'column' => 'id')),
+      'user_id'     => new sfValidatorPropelChoice(array('required' => false, 'model' => 'sfGuardUser', 'column' => 'id')),
+      'admin'       => new sfValidatorChoice(array('required' => false, 'choices' => array('', 1, 0))),
+    ));
 
-		$this->widgetSchema->setNameFormat('sfoauthserverdeveloper_filters[%s]');
+    $this->widgetSchema->setNameFormat('sfoauthserverdeveloper_filters[%s]');
 
-		$this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
 
-		parent::setup();
-	}
+    parent::setup();
+  }
 
-	public function getModelName()
-	{
-		return 'Sfoauthserverdeveloper';
-	}
+  public function getModelName()
+  {
+    return 'Sfoauthserverdeveloper';
+  }
 
-	public function getFields()
-	{
-		return array(
-			'consumer_id' => 'ForeignKey',
-			'user_id' => 'ForeignKey',
-			'admin' => 'Boolean',
-			'id' => 'Number',
-		);
-	}
+  public function getFields()
+  {
+    return array(
+      'consumer_id' => 'ForeignKey',
+      'user_id'     => 'ForeignKey',
+      'admin'       => 'Boolean',
+      'id'          => 'Number',
+    );
+  }
 }
--- lib/filter/base/BaseSfoauthserveruserscopeFormFilter.class.php
+++ lib/filter/base/BaseSfoauthserveruserscopeFormFilter.class.php
@@ -3,45 +3,45 @@
 /**
  * Sfoauthserveruserscope filter form base class.
  *
- * @package	##PROJECT_NAME##
+ * @package    ##PROJECT_NAME##
  * @subpackage filter
- * @author	 ##AUTHOR_NAME##
+ * @author     ##AUTHOR_NAME##
  */
 abstract class BaseSfoauthserveruserscopeFormFilter extends BaseFormFilterPropel
 {
-	public function setup()
-	{
-		$this->setWidgets(array(
-							   'user_id' => new sfWidgetFormPropelChoice(array('model' => 'sfGuardUser', 'add_empty' => true)),
-							   'consumer_id' => new sfWidgetFormPropelChoice(array('model' => 'Sfoauthserverconsumer', 'add_empty' => true)),
-							   'scope' => new sfWidgetFormFilterInput(),
-						  ));
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'user_id'     => new sfWidgetFormPropelChoice(array('model' => 'sfGuardUser', 'add_empty' => true)),
+      'consumer_id' => new sfWidgetFormPropelChoice(array('model' => 'Sfoauthserverconsumer', 'add_empty' => true)),
+      'scope'       => new sfWidgetFormFilterInput(),
+    ));
 
-		$this->setValidators(array(
-								  'user_id' => new sfValidatorPropelChoice(array('required' => false, 'model' => 'sfGuardUser', 'column' => 'id')),
-								  'consumer_id' => new sfValidatorPropelChoice(array('required' => false, 'model' => 'Sfoauthserverconsumer', 'column' => 'id')),
-								  'scope' => new sfValidatorPass(array('required' => false)),
-							 ));
+    $this->setValidators(array(
+      'user_id'     => new sfValidatorPropelChoice(array('required' => false, 'model' => 'sfGuardUser', 'column' => 'id')),
+      'consumer_id' => new sfValidatorPropelChoice(array('required' => false, 'model' => 'Sfoauthserverconsumer', 'column' => 'id')),
+      'scope'       => new sfValidatorPass(array('required' => false)),
+    ));
 
-		$this->widgetSchema->setNameFormat('sfoauthserveruserscope_filters[%s]');
+    $this->widgetSchema->setNameFormat('sfoauthserveruserscope_filters[%s]');
 
-		$this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
 
-		parent::setup();
-	}
+    parent::setup();
+  }
 
-	public function getModelName()
-	{
-		return 'Sfoauthserveruserscope';
-	}
+  public function getModelName()
+  {
+    return 'Sfoauthserveruserscope';
+  }
 
-	public function getFields()
-	{
-		return array(
-			'user_id' => 'ForeignKey',
-			'consumer_id' => 'ForeignKey',
-			'scope' => 'Text',
-			'id' => 'Number',
-		);
-	}
+  public function getFields()
+  {
+    return array(
+      'user_id'     => 'ForeignKey',
+      'consumer_id' => 'ForeignKey',
+      'scope'       => 'Text',
+      'id'          => 'Number',
+    );
+  }
 }
